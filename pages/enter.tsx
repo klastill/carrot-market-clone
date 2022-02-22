7547ee0f559a257ecf332748cf2b8c88507c2967
klastill <gyb0501@gmail.com>
2022-02-22 11:29:44 +0900

    form submission

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -11,6 +11,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [submitting, setSubmitting] = useState(false);
   const { register, watch, handleSubmit, reset } = useForm();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
@@ -22,7 +23,14 @@ const Enter: NextPage = () => {
     reset();
   };
   const onValid = (data: EnterForm) => {
-    console.log(data)
+    setSubmitting(true)
+    fetch("/api/users/enter", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then(() => {setSubmitting(false)})
   }
   return (
     <div className="mt-16 px-4">
@@ -79,9 +87,9 @@ const Enter: NextPage = () => {
               required
             />
           ) : null}
-          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "email" ? <Button text={submitting ? "Loading" : "Get login link"} /> : null}
           {method === "phone" ? (
-            <Button text={"Get one-time password"} />
+            <Button text={submitting ? "Loading" : "Get one-time password"} />
           ) : null}
         </form>
 
